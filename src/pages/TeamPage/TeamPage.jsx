fda0779187ba8a5412c18820f3d4c378fbba4e30
iara baya <iarabayav@gmail.com>
2022-07-07 16:39:24 -0300

    alert for action message added

--- src/pages/TeamPage/TeamPage.jsx
+++ src/pages/TeamPage/TeamPage.jsx
@@ -1,13 +1,16 @@
+import { useState } from 'react';
+import Alert from '../../components/alert/Alert';
 import TeamCard from '../../components/cards/TeamCard';
 import usePokemons from '../../hooks/usePokemons';
 
 const TeamPage = () => {
-
+	const [alert, setAlert] = useState();
 	const { team } = usePokemons();
 
 	if (team.length >= 1) {
 		return (
-			<div className='max-w-4xl min-h-full flex mx-auto mb-4 p-1 md:p-12 rounded-xl shadow-2xl items-center justify-center'>
+			<div className='max-w-4xl min-h-full flex flex-col mx-auto mb-4 p-1 md:p-12 rounded-xl shadow-2xl items-center justify-center'>
+				{alert && <Alert type="delete" message="You've deleted from your team"/>}
 				<div className='grid gap-4 grid-cols-1 place-content-center md:md:grid-cols-3 xl:grid-cols-3'>
 					{
 						team.map((pokemon, index) => (<TeamCard key={index} name={pokemon.name} />))
