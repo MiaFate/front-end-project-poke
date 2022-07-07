32c2910f90e139fa6d22ee7ad79f8a666e298ac7
iara baya <iarabayav@gmail.com>
2022-07-07 19:09:55 -0300

    landing button patch

--- src/pages/LandingPage/LandingPage.jsx
+++ src/pages/LandingPage/LandingPage.jsx
@@ -1,8 +1,16 @@
-import React from 'react'
+import { Link } from 'react-router-dom'
 
 const LandingPage = () => {
 	return (
-		<div>LandingPage</div>
+		<div>
+			in construction ....
+			<h1 className='font-bold'>LandingPage</h1>
+			<Link to='/home'>
+				<button type='button'  className="bg-red-500 hover:bg-red-700 text-white font-bold my-2 py-2 px-4 rounded">
+						Go Home!
+				</button>
+			</Link>
+		</div>
 	)
 }
 
