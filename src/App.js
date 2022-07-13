44bbc915a64191977fc3296f01c1ea56b45af395
MiaFate <miapurplefroot@gmail.com>
2022-07-13 15:33:44 -0300

    se le agrego un setTimeout a los import de HomePage y LandingPage en App.js para que duren mas los loaders con Suspense

--- src/App.js
+++ src/App.js
@@ -9,13 +9,24 @@ import ProfilePage from './pages/ProfilePage/ProfilePage';
 import TeamPage from './pages/TeamPage/TeamPage';
 import ProtectedRoutes from './containers/Protected/ProtectedRoute';
 import Loading from './components/loading/Loading';
-const HomePage = lazy(() => import('./pages/HomePage/HomePage'));
+const HomePage = lazy(async () => {
+	const [moduleExports] = await Promise.all([
+		import('./pages/HomePage/HomePage'),
+		new Promise(resolve => setTimeout(resolve, 500))
+	]);
+	return moduleExports;
+});
 const NotFoundPage = lazy(() => import('./pages/NotFoundPage/NotFoundPage'));
 const PokemonPage = lazy(() => import('./pages/PokemonPage/PokemonPage'));
-// import PokemonPage from './pages/PokemonPage/PokemonPage';
 
 //public pages
-const LandingPage = lazy(() => import('./pages/LandingPage/LandingPage'));
+const LandingPage = lazy(async () => {
+	const [moduleExports] = await Promise.all([
+		import('./pages/LandingPage/LandingPage'),
+		new Promise(resolve => setTimeout(resolve, 1000))
+	]);
+	return moduleExports;
+});
 const SignUpPage = lazy(() => import('./pages/SignUpPage/SignUpPage'));
 const PassResetPage = lazy(() => import('./pages/PassResetPage/PassResetPage'));
 
@@ -24,7 +35,7 @@ function App() {
 		<BrowserRouter>
 			<Routes>
 
-				<Route path="/notfound" element={<NotFoundPage/>} />
+				<Route path="/notfound" element={<NotFoundPage />} />
 				<Route path="*" element={<Navigate replace to="/notfound" />} />
 				<Route path='/' element={<Suspense fallback={<Loading />}><LandingPage /></Suspense>} />
 				<Route path='/signup' element={<Suspense fallback={<Loading />}><SignUpPage /></Suspense>} />
